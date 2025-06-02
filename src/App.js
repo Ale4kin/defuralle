a2fa11b73d4fce94226784daf6026bc3e705fec3
Ale4kin <51973254+Ale4kin@users.noreply.github.com>
2025-06-02 11:15:42 +0200

    Change Router from BrowserRouter to HashRouter in App.js for improved routing compatibility.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
